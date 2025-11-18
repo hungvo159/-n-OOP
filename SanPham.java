43ebde1181a654921948849ee627e7f34e0dba07
hungvo159 <nghiephung8277@gmail.com>
2025-11-18 20:36:52 +0700

    Update SanPham.java

--- SanPham.java
+++ SanPham.java
@@ -6,7 +6,6 @@ abstract class SanPham implements IFile {
     protected String ten;
     protected int soLuong;
     protected double donGia;
-
     public SanPham() {
         this.ma = "";
         this.ten = "";
@@ -32,9 +31,9 @@ abstract class SanPham implements IFile {
 
     @Override
     public void nhap() {
-        ma = QuanLyKho.readString("Nhập mã sản phẩm: ");
+        // Mã sẽ được nhập ở lớp con
         ten = QuanLyKho.readString("Nhập tên sản phẩm: ");
-        soLuong = QuanLyKho.readInt("Nhập số lượng: ", 0);
+        soLuong = QuanLyKho.readInt("Nhập số lượng: ", 1); 
         donGia = QuanLyKho.readDouble("Nhập đơn giá: ", 0.0);
     }
 
@@ -45,99 +44,111 @@ abstract class SanPham implements IFile {
 
     public abstract double tinhGiaTri();
     public abstract String toFileString(); 
+
+//tao mot ban sao moi de luu tru san pham 
+    public abstract SanPham cloneWithNewQuantity(int newQuantity);
 }
 
-class Food extends SanPham {
-    protected Date hanSuDung;
-
-    public Food() { super(); hanSuDung = new Date(System.currentTimeMillis()); }
-
-    public Food(String ma, String ten, int soLuong, double donGia, Date hanSuDung) {
-        super(ma, ten, soLuong, donGia);
-        this.hanSuDung = hanSuDung;
-    }
-
-    public Date getHanSuDung() { return hanSuDung; }
-    public void setHanSuDung(Date hanSuDung) { this.hanSuDung = hanSuDung; }
-
-    @Override
-    public void nhap() {
-        super.nhap();
-        java.util.Date hsdUtil = QuanLyKho.readDate_YYYYMMDD("Nhập hạn sử dụng (yyyy-mm-dd): ");
-        this.hanSuDung = new java.sql.Date(hsdUtil.getTime()); 
-    }
-
-    @Override
-    public void xuat() {
-        super.xuat();
-        System.out.println("HSD: " + hanSuDung);
-    }
-
-    @Override
-    public double tinhGiaTri() { return soLuong * donGia; }
-    
-    @Override
-    public String toFileString() { return ""; }
-}
-
-class ThucAn extends Food {
+class ThucAn extends SanPham {
+    protected Date hanSuDung; 
     private String dangBaoBi;
     private static final String[] DANG_BAO_BI_OPTIONS = {"Hop", "Goi"};
-
-    public ThucAn() { super(); dangBaoBi = ""; }
+    public ThucAn() { 
+        super(); 
+        dangBaoBi = ""; 
+        hanSuDung = new Date(System.currentTimeMillis()); 
+    }
 
     public ThucAn(String ma, String ten, int soLuong, double donGia, Date hanSuDung, String dangBaoBi) {
-        super(ma, ten, soLuong, donGia, hanSuDung);
+        super(ma, ten, soLuong, donGia);
+        this.hanSuDung = hanSuDung;
         this.dangBaoBi = dangBaoBi;
     }
 
     public String getDangBaoBi() { return dangBaoBi; }
     public void setDangBaoBi(String dangBaoBi) { this.dangBaoBi = dangBaoBi; }
+    public Date getHanSuDung() { return hanSuDung; }
+    public void setHanSuDung(Date hanSuDung) { this.hanSuDung = hanSuDung; }
+
 
     @Override
     public void nhap() {
-        super.nhap();
-        dangBaoBi = QuanLyKho.readStringWithOptions("Nhập dạng bao bì (Hop/Goi): ", DANG_BAO_BI_OPTIONS);    }
+        ma = QuanLyKho.readMa("Nhập mã thức ăn (TAXXX): ", "TA"); 
+        super.nhap(); 
+        java.util.Date hsdUtil = QuanLyKho.readDate_HSD("Nhập hạn sử dụng (yyyy-mm-dd): "); 
+        this.hanSuDung = new java.sql.Date(hsdUtil.getTime()); 
+        dangBaoBi = QuanLyKho.readStringWithOptions("Nhập dạng bao bì (Hop/Goi): ", DANG_BAO_BI_OPTIONS);
+    }
 
     @Override
     public void xuat() {
         super.xuat();
-        System.out.println("Dạng bao bì: " + dangBaoBi);
+        System.out.println("  HSD: " + hanSuDung + " | Dạng bao bì: " + dangBaoBi);
     }
+
+    @Override
+    public double tinhGiaTri() { return soLuong * donGia; }
 
     @Override
     public String toFileString() {
         return "ThucAn|" + ma + "|" + ten + "|" + soLuong + "|" + donGia + "|" + hanSuDung + "|" + dangBaoBi;
     }
+
+    @Override
+    public SanPham cloneWithNewQuantity(int newQuantity) {
+        return new ThucAn(this.ma, this.ten, newQuantity, this.donGia, this.hanSuDung, this.dangBaoBi);
+    }
 }
 
-class NuocUong extends Food {
+class NuocUong extends SanPham {
+    protected Date hanSuDung;
     private String loaiVo;
     private static final String[] LOAI_VO_OPTIONS = {"Lon", "Chai"};
-    public NuocUong() { super(); loaiVo = ""; }
+
+    public NuocUong() { 
+        super(); 
+        loaiVo = ""; 
+        hanSuDung = new Date(System.currentTimeMillis());
+    }
+    
     public NuocUong(String ma, String ten, int soLuong, double donGia, Date hanSuDung, String loaiVo) {
-        super(ma, ten, soLuong, donGia, hanSuDung);
+        super(ma, ten, soLuong, donGia);
+        this.hanSuDung = hanSuDung;
         this.loaiVo = loaiVo;
     }
 
     public String getLoaiVo() { return loaiVo; }
     public void setLoaiVo(String loaiVo) { this.loaiVo = loaiVo; }
+    public Date getHanSuDung() { return hanSuDung; }
+    public void setHanSuDung(Date hanSuDung) { this.hanSuDung = hanSuDung; }
 
     @Override
     public void nhap() {
-        super.nhap();
-         loaiVo = QuanLyKho.readStringWithOptions("Nhập loại vỏ (Lon/Chai): ", LOAI_VO_OPTIONS);    
+        ma = QuanLyKho.readMa("Nhập mã nước uống (NUXXX): ", "NU"); 
+        super.nhap(); 
+        java.util.Date hsdUtil = QuanLyKho.readDate_HSD("Nhập hạn sử dụng (yyyy-mm-dd): "); 
+        this.hanSuDung = new java.sql.Date(hsdUtil.getTime()); 
+        loaiVo = QuanLyKho.readStringWithOptions("Nhập loại vỏ (Lon/Chai): ", LOAI_VO_OPTIONS);    
     }
 
     @Override
     public void xuat() {
         super.xuat();
-        System.out.println("Loại vỏ: " + loaiVo);
+        System.out.println("  HSD: " + hanSuDung + " | Loại vỏ: " + loaiVo);
     }
+
+    @Override
+    public double tinhGiaTri() { return soLuong * donGia; }
 
     @Override
     public String toFileString() {
         return "NuocUong|" + ma + "|" + ten + "|" + soLuong + "|" + donGia + "|" + hanSuDung + "|" + loaiVo;
+    }
+
+//tạo clone 
+    @Override
+    public SanPham cloneWithNewQuantity(int newQuantity) {
+        return new NuocUong(this.ma, this.ten, newQuantity, this.donGia, this.hanSuDung, this.loaiVo);
     }
 }
 
@@ -154,13 +165,15 @@ class NonFood extends SanPham {
 
     @Override
     public void nhap() {
-        super.nhap();
-        chatLieu = QuanLyKho.readString("Nhập chất liệu: ");    }
+        ma = QuanLyKho.readMa("Nhập mã NonFood (NFXXX): ", "NF"); 
+        super.nhap(); 
+        chatLieu = QuanLyKho.readString("Nhập chất liệu: ");    
+    }
 
     @Override
     public void xuat() {
         super.xuat();
-        System.out.println("Chất liệu: " + chatLieu);
+        System.out.println("  Chất liệu: " + chatLieu);
     }
 
     @Override
@@ -169,5 +182,10 @@ class NonFood extends SanPham {
     @Override
     public String toFileString() {
         return "NonFood|" + ma + "|" + ten + "|" + soLuong + "|" + donGia + "|" + chatLieu;
+    }
+    
+    @Override
+    public SanPham cloneWithNewQuantity(int newQuantity) {
+        return new NonFood(this.ma, this.ten, newQuantity, this.donGia, this.chatLieu);
     }
 }
